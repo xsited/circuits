e922dcf1900199fba81cc48a791c4f9db7bfb097
Thomas Kee <xsited@yahoo.com>
2014-07-15 14:14:40 -0700

    Overload with compute methods and add placeholder for rpc callouts

--- o.py
+++ o.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+'''
+
+
+
+
+'''
+
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.ext.httpauth import HTTPBasicAuth
  
@@ -50,7 +57,7 @@ def make_circuit(circuit):
     new_circuit = {}
     for field in circuit:
         if field == 'id':
-            new_circuit['uri'] = url_for('get_circuit', circuit_id = curcuit['id'], _external = True)
+            new_circuit['uri'] = url_for('get_circuit', circuit_id = circuit['id'], _external = True)
         else:
             new_circuit[field] = circuit[field]
     return new_circuit
@@ -81,6 +88,19 @@ def create_circuit():
         'active': False
     }
     circuits.append(circuit)
+    '''
+    # Call OVSDB json-rpc  to configure TEP
+  
+    rpc_ovsdb_create_tep()	
+    Compute node:
+    start_ip = 192.168.1.182
+    end_ip = 192.168.1.183
+    ovs-vsctl add-port br0 vxlan -- set Interface vxlan type=vxlan options:key=10 options:local_ip=192.168.1.183 options:remote_ip=192.168.1.182  ofport_request=10
+    ovs-vsctl add-port br0 vxlan -- set Interface vxlan type=vxlan options:key=10 options:local_ip=192.168.1.182 options:remote_ip=192.168.1.183 ofport_request=10
+    json-rpc client call
+    rpc_faultmonitor_start(id, start_ip_address, end_ip_address);
+
+    '''
     return jsonify( { 'circuit': make_circuits(circuits) } ), 201
  
 @app.route('/orchestrator/api/v1.0/circuits/<int:circuit_id>', methods = ['PUT'])
@@ -112,6 +132,42 @@ def delete_circuits(circuit_id):
         abort(404)
     circuits.remove(circuit[0])
     return jsonify( { 'result': True } )
+
+@app.route('/compute/api/v1.0/faultmonitor/<int:circuit_id>', methods = ['POST'])
+# @auth.login_required
+def compute_faultmonitor_start(circuit_id, start_ip, end_ip):
+    circuit = filter(lambda t: t['id'] == circuit_id, circuits)
+    if len(circuit) == 0:
+        abort(404)
+    # ping_start (end_ip);
+    return jsonify( { 'result': True } )
+    
+@app.route('/compute/api/v1.0/performancemetrics/<int:circuit_id>', methods = ['GET'])
+# @auth.login_required
+def compute_performance_metrics_get(circuit_id):
+    circuit = filter(lambda t: t['id'] == circuit_id, circuits)
+    if len(circuit) == 0:
+        abort(404)
+    return jsonify( { 'circuit': make_circuit_metrics(circuit_id, latency, throughput) } ), 201
+    
+'''
+Not needed at the moment ovsdb
+
+@app.route('/compute/api/v1.0/circuits', methods = ['POST'])
+# @auth.login_required
+def compute_circuit_create():
+    if not request.json or not 'service_type' in request.json:
+        abort(400)
+    circuit = {
+        'id': circuits[-1]['id'] + 1,
+        'service_type': request.json['service_type'],
+        'start_ip_address': request.json.get('start_ip_address', ""),
+        'end_ip_address': request.json.get('end_ip_address', ""),
+        'active': True
+    }
+    return jsonify( { 'circuit': make_circuits(circuits) } ), 201
+'''
+
     
 if __name__ == '__main__':
     app.run(debug = True)
