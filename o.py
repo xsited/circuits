dd18c8fe526dc14b483c9fbb56f7c79cc37ae9a3
CableLabs <xsited@yahoo.com>
2014-07-16 13:38:39 -0600

    Unit test path corrections

--- o.py
+++ o.py
@@ -190,13 +190,15 @@ if __name__ == '__main__':
 '''
 Request methods using CURL:
  
-1. curl -i -u user:password http://localhost:5555/orchestration/api/v1.0/circuits
+0. curl -i -u user:password http://localhost:5555/orchestrator/api/v1.0/hello
+
+1. curl -i -u user:password http://localhost:5555/orchestrator/api/v1.0/circuits
  
-2. curl -i -u user:password http://localhost:5555/orchestration/api/v1.0/circuits/2
+2. curl -i -u user:password http://localhost:5555/orchestrator/api/v1.0/circuits/2
  
-3. curl -i -u user:password -H "Content-Type: application/json" -X POST -d '{ "service_type": "epl", "start_ip_ address": "192.168.1.3", "end_ip_address": "192.168.1.4", "classifier": "red" }' http://localhost:5555/orchestration/api/v1.0/circuits
+3. curl -i -u user:password -H "Content-Type: application/json" -X POST -d '{ "service_type": "epl", "start_ip_ address": "192.168.1.3", "end_ip_address": "192.168.1.4", "classifier": "red" }' http://localhost:5555/orchestrator/api/v1.0/circuits
  
-4. curl -i -u user:password -H "Content-Type: application/json" -X PUT -d '{"active":true}' http://localhost:5555/orchestration/api/v1.0/circuits/2
+4. curl -i -u user:password -H "Content-Type: application/json" -X PUT -d '{"active":true}' http://localhost:5555/orchestrator/api/v1.0/circuits/2
  
 '''
 
